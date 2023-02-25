2baa29ee51a2d5d49ebf2cd72d122d31849f731a
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2023-02-25 21:49:06 +0530

    Add files via upload

---
+++ Assign(24-2-2023).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int num;
+   cout << "Enter a number: ";
+   cin >> num;
+
+   cout << "Even numbers from 0 to " << num << " are: ";
+   for (int i = 0; i <= num; i += 2) {
+      cout << i << " ";
+   }
+   cout << endl;
+
+return 0;
+}
